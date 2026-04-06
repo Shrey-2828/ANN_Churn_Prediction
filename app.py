fcbce5d50943c5378260e01128747e23ad65896c
Shrey-2828 <ldce.2324@gmail.com>
2026-04-06 23:53:37 +0530

    ANN training done

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 import numpy as np
+import steamlit as st
+import joblib
+
+# Page configuration
+st.set_page_config(
+    page_title="Titanic Survival Predictor",
+    page_icon="🚢",
+    layout="centered"
+)
+
+model=joblib.load('model.sav')
 
 def predict(data):
-    input=np.arr[data]
+    ans=model.predict(data)
+    return ans[0]
+
+st.title("Customer Churn Prediction..")
+
+Credit_Score = st.number_input('Credit_score',min_value=0.0)
+Age = st.number_input('Age',min_value=0,max_value=110)
+Tenure = st.number_input('Tenure',)
+
+    
+    
